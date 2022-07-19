52f34e9be57330ed6309f90a84e14309fd88428f
Niels Wijers <nielswijers@gmail.com>
2022-07-19 13:57:17 +0200

    add tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,9 @@
 export default function Index() {
-  return <div>App</div>
+  return (
+    <div className="m-4">
+      <div className="grid place-content-center h-60 rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        <h1 className="text-4xl font-bold text-white">Dev stack</h1>
+      </div>
+    </div>
+  )
 }
